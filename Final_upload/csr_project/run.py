ee6991c0b43f7bbe966727fc707cdb518dc78984
AdarshMukherjee21 <151902097+AdarshMukherjee21@users.noreply.github.com>
2025-02-09 09:47:18 +0530

    Delete Final_upload directory

--- Final_upload/csr_project/run.py
+++
@@ -1,4 +0,0 @@
-from app.routes_ai import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- Final_upload/test.py
+++
@@ -1,4 +0,0 @@
-import pymongo
-from bson import ObjectId
-
-print("PyMongo and BSON are working!")
